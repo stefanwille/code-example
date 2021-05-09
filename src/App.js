c4704afe79f3d4037d42b54df9c31c49d0afdb8c
Stefan Wille <github@stefanwille.com>
2021-05-09 15:44:14 +0200

    Make app more compelling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./antd-theme/antd-customized.css";
-import { Button, Space } from "antd";
+import { Button, Input, Switch, Space } from "antd";
 
 function App() {
   return (
     <Space style={{ margin: 50 }}>
       <Button type="primary">Button</Button>
-      <Button>Another Button</Button>
+      <Input.Search placeholder="Input search text" />
+      <Switch defaultChecked />
     </Space>
   );
 }
